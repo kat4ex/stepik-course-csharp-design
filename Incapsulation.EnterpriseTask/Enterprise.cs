d74d717bcaf247fa70efa75c2a915a0d679eece2
Kat4ex <hepshot@yandex.ru>
2021-09-25 19:46:57 +0500

    added Incapsulation.EnterpriseTask project

---
+++ Incapsulation.EnterpriseTask/Enterprise.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Incapsulation.EnterpriseTask
+{
+    public class Enterprise
+    {
+        private string _inn;
+        public Guid Guid { get; }
+        public string Name { get; set; }
+
+        public string Inn
+        {
+            get => _inn;
+            set
+            {
+                if (value.Length != 10 || !value.All(char.IsDigit))
+                    throw new ArgumentException();
+                _inn = value;
+            }
+        }
+
+        public DateTime EstablishDate { get; set; }
+
+        public TimeSpan ActiveTimeSpan => DateTime.Now - EstablishDate;
+
+        public Enterprise(Guid guid)
+        {
+            Guid = guid;
+        }
+        
+        public double GetTotalTransactionsAmount()
+        {
+            DataBase.OpenConnection();
+            var amount = 0.0;
+            foreach (var t in DataBase.Transactions().Where(z => z.EnterpriseGuid == Guid))
+                amount += t.Amount;
+            return amount;
+        }
+    }
+}
