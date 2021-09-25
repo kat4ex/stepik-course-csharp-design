cd04e1b2171d5a8a1f1cc3d253f3347056c7b96c
Kat4ex <hepshot@yandex.ru>
2021-09-25 20:43:11 +0500

    added a solution to the Incapsulation.Weights

--- Incapsulation.Weights/Indexer.cs
+++ Incapsulation.Weights/Indexer.cs
@@ -1,10 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Incapsulation.Weights
 {
-	
+    public class Indexer
+    {
+        private readonly double[] _data;
+        private readonly int _start;
+
+        public Indexer(double[] data, int start, int length)
+        {
+            if (start < 0 || length < 0)
+                throw new ArgumentException();
+
+            if (start + length > data.Length)
+                throw new ArgumentException();
+
+            _data = data;
+            _start = start;
+            Length = length;
+        }
+
+        public int Length { get; }
+
+        public double this[int index]
+        {
+            get
+            {
+                var realIndex = GetRealIndex(index);
+
+                if (IsIndexValid(realIndex))
+                    return _data[realIndex];
+
+                throw new IndexOutOfRangeException();
+            }
+
+            set
+            {
+                var realIndex = GetRealIndex(index);
+
+                if (IsIndexValid(realIndex))
+                {
+                    _data[realIndex] = value;
+                }
+                else throw new IndexOutOfRangeException();
+            }
+        }
+
+        private int GetRealIndex(int index)
+        {
+            return index + _start;
+        }
+
+        private bool IsIndexValid(int index)
+        {
+            return !(index > Length || index < _start);
+        }
+    }
 }
